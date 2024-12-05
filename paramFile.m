d7f085e8ffc01723ab9b5c95fb00f815d61cf36f
Chackben2021 <chackben2021@gmail.com>
2024-12-05 14:35:29 -0800

    4
    4

--- paramFile.m
+++ paramFile.m
@@ -16,30 +16,31 @@ paramWaveformFMCWM.Slope = paramWaveformFMCWM.Bw/paramWaveformFMCWM.T;
 paramWaveformFMCWM.NumSweeps = 64;
 
 %Transmiter Parameters
-paramTransmiter.P = 0.00316227766016838; % Watts - Power
+paramTransmiter.P = 0.00316; % Watts - Power
 paramTransmiter.Av = 36; % dB - Gain
 
 
 %FreeSpace Parameters 
-paramFreeSpace.maxDist = 10; % meters - Maximum one way distance
+paramFreeSpace.maxDist = 200; % meters - Maximum one way distance
+
 
 
 %Physical Parameters
 paramGeometry.RadarVel = [0; 0; 0]; % meters - Radars position
 paramGeometry.RadarPos = [0; 0; 0]; % meters/sec - Radars velocity
 
-paramGeometry.TargetVel = [2.5; 0; 0]; % meters - Targets position
-paramGeometry.TargetPos = [5.25; 0; 0]; % meters/sec - Targets velocity
+paramGeometry.TargetVel = [4; 0; 0]; % 
+paramGeometry.TargetPos = [9; 0; 0]; %
 paramGeometry.TargetArea = 5; % m^2 -  Mean radar cross section 
 
 %Reciver Parameters
 paramReciver.Av = 42; % dB - Gain
-paramReciver.Nf = 4.5; % dB - Noise Figure
+paramReciver.Nf = 0; % dB - Noise Figure
 paramReciver.Temp = 300; % kelvin - Reference temperature
 
 
 % Range Processing Interval
-rangeProcessLimits = [1 15]; % Only process from 1 m to 200 m 
+rangeProcessLimits = [0.1 200]; % Only process from 1 m to 200 m 
 rngVec = beat2range(fftshiftfreqgrid(paramWaveformFMCWM.NumSamplesSweep,paramWaveformFMCWM.Fs),...
                             paramWaveformFMCWM.Slope,c);
 [~,idxRangeProcessMin] = min(abs(rngVec - rangeProcessLimits(1))); 
@@ -68,18 +69,16 @@ idxRngCUT = numCUTRng:(numRng-numCUTRng+1);
 idxDopCUT = numCUTDop:(numDop-numCUTDop+1);
 paramDetection.NumCUTIdx = numel(idxRngCUT).*numel(idxDopCUT); 
 
+%R/D Ploting
+%paramDetection.RngLims = [rngVec(idxRangeProcessMin) rngVec(idxRangeProcessMax)]; % m
+paramDetection.RngLims = [0 10];
+dopVec = fftshiftfreqgrid(paramDetection.NumDop,paramRadarWaveformFMCWMT.PRF); % Hz
+speedVec = sort(-dop2speed(dopVec,paramRadarHardwareFMCWMT.lambda)/2); % m/s
+%paramDetection.SpeedLims = [speedVec(1) speedVec(end)]; % m/s
+paramDetection.SpeedLims = [-3 3];
 
 function freq_grid = fftshiftfreqgrid(N,Fs)
-%fftshiftfreqgrid   Generate frequency grid
-%   freq_grid = fftshiftfreqgrid(N,Fs) generate an N point
-%   frequency grid according to sample rate Fs. This grid matches
-%   the operation used in fftshift.
-%
-%   % Example:
-%   %   Create a 16 point frequency grid for a sample rate of 10
-%   %   Hz.
-%
-%   freq_grid = fftshiftfreqgrid(16,10)
+
 freq_res = Fs/N;
 freq_grid = (0:N-1).'*freq_res;
 Nyq = Fs/2;
